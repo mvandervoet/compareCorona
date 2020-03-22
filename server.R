62ede3667a4444c59c735113396724a073cf10f9
Monique van der Voet <monique@VivaltesPro.local>
2020-03-22 16:54:11 +0100

    commit

---
+++ server.R
@@ -0,0 +1,165 @@
+# Project: Compare Corona virus outbreak between countries
+# Author: Monique van der Voet
+
+library("shiny")
+library("httr")
+library("shinyWidgets")
+library("readr")
+library("dplyr")
+library("readxl")
+library("ggpubr")
+
+# Load lookup tables
+appSelectizeLookupNL <- read_csv("data/appSelectizeLookupNL.csv")
+appSelectizeLookupCN <- read_csv("data/appSelectizeLookupCN.csv")
+
+server <- function(input, output, session) {
+  # Create Selectize toggle box -----------------------------------------------------------------------
+  updateSelectizeInput(session,
+                       'ref_id',
+                       choices = appSelectizeLookupNL,
+                       server = TRUE)
+  
+  output$values_ref_id <- renderText({
+    paste("You chose: ", input$ref_id)  # This text is printed in the app
+  })
+  
+  updateSelectizeInput(session,
+                       'query_id',
+                       choices = appSelectizeLookupCN,
+                       server = TRUE)
+  
+  output$values_query_id <- renderText({
+    paste("You chose: ", input$query_id)  # This text is printed in the app
+  })
+  
+  updateSelectizeInput(session,
+                       'ref_date',
+                       choices = data.frame(value = c(seq(
+                         as.Date("2019-12-30"), as.Date(format(Sys.time(), "%Y-%m-%d")), "days"
+                       )),
+                       label = c(seq(
+                         as.Date("2019-12-30"), as.Date(format(Sys.time(), "%Y-%m-%d")), "days"
+                       ))),
+                       server = TRUE)
+  
+  output$values_ref_date <- renderText({
+    paste("You chose: ", input$ref_date)  # This text is printed in the app
+  })
+  
+  updateSelectizeInput(session,
+                       'query_date',
+                       choices = data.frame(value = c(seq(
+                         as.Date("2019-12-30"), as.Date(format(Sys.time(), "%Y-%m-%d")), "days"
+                       )),
+                       label = c(seq(
+                         as.Date("2019-12-30"), as.Date(format(Sys.time(), "%Y-%m-%d")), "days"
+                       ))),
+                       server = TRUE)
+  
+  output$values_query_date <- renderText({
+    paste("You chose: ", input$query_date)  # This text is printed in the app
+  })
+  
+  
+  # Create Graph -----------------------------------------------------------------------
+  observeEvent(input$goCompare, {
+    showNotification(
+      "I'm working on your query! This will take a few seconds.",
+      duration = 2,
+      type = "message"
+    )
+    
+    input$ref_id
+    input$query_id
+    input$ref_date
+    input$query_date
+    
+    output$plotCompare <- renderPlot({
+      req(input$ref_id)
+      req(input$query_id)
+      req(input$ref_date)
+      req(input$query_date)
+      
+      # Define Variables
+      outbreakCurrent <- input$ref_id # "NL"
+      outbreakPast <- input$query_id # "CN"
+      
+      outbreakCurrentlockdown <-
+        input$ref_date  %>% as.Date() # "2020-03-15"
+      outbreakPastlockdown <-
+        input$query_date  %>% as.Date() # "2020-01-23"
+      
+      # Create the URL where the dataset is stored with automatic updates every day
+      urlToday <-
+        paste(
+          "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-",
+          format(Sys.time(), "%Y-%m-%d"),
+          ".xlsx",
+          sep = ""
+        )
+      urlYesterday <-
+        paste(
+          "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-",
+          format(Sys.Date() - 1, "%Y-%m-%d"),
+          ".xlsx",
+          sep = ""
+        )
+      
+      # Check status code, whether a current daily update is available otherwise take url from previous day
+      response <- GET(urlToday, authenticate(":", ":", type = "ntlm"))
+      if (response$status_code == 200) {
+        url = urlToday
+      } else {
+        url = urlYesterday
+      }
+      
+      # Download the dataset from the website to a local temporary file
+      response <-
+        GET(url,
+            authenticate(":", ":", type = "ntlm"),
+            write_disk(tf <- tempfile(fileext = ".xlsx")))
+      
+      # Load the full dataset
+      data <- read_excel(tf) %>%
+        mutate(DateRep = (DateRep - lubridate::days(1))) # correct for a one-day delay in reporting
+      names(data)[names(data) == 'Countries and territories'] <-
+        'Country'
+      
+      # Process data
+      datasubset <-
+        subset(data, GeoId %in% c(outbreakCurrent, outbreakPast))
+      offset <-
+        difftime(outbreakCurrentlockdown, outbreakPastlockdown, units = 'days') %>% as.numeric
+      
+      datasubset[datasubset$GeoId == "CN", ] <-
+        mutate(datasubset[datasubset$GeoId == "CN", ], DateRep = (DateRep + lubridate::days(offset)))
+      
+      datasubset$Date <- as.Date(datasubset$DateRep)
+      
+      plot <- ggline(
+        datasubset,
+        x = "DateRep",
+        y = "Deaths",
+        point.size = 0.1,
+        #xscale = ,
+        color = 'Country',
+        palette = c("#E7B800", "#00AFBB"),
+        xlab = paste(
+          "\nDate (",
+          outbreakPast,
+          " was offset by ",
+          offset,
+          " days)",
+          sep = ""
+        ),
+        ylab = "Deaths per day"
+      )
+      plot
+      
+    },)
+    
+  })
+  
+  # End
+}
---
+++ ui.R
@@ -0,0 +1,99 @@
+# Project: Compare Corona virus outbreak between countries
+# Author: Monique van der Voet
+
+ui <- fluidPage(
+  
+  h3("Compare Corona virus outbreak between countries"),
+  p(
+    em(
+      "You can offset the timelines to overlap milestone events, for example lockdowns"
+    )
+  ),
+  
+  wellPanel(
+    fluidRow(column(
+      4,
+      selectizeInput(
+        inputId = 'ref_id',
+        # do not change name
+        label = 'Select first country:',
+        choices = NULL,
+        options = list(
+          placeholder = 'e.g. Netherlands',
+          onInitialize = I('function() { this.setValue(""); }')
+        )
+      )
+    ),
+    column(
+      4,
+      selectizeInput(
+        inputId = 'query_id',
+        # do not change name
+        label = 'Select second country:',
+        choices = NULL,
+        options = list(
+          placeholder = 'e.g. China',
+          onInitialize = I('function() { this.setValue(""); }')
+        )
+      )
+    )),
+    
+    fluidRow(column(
+      4,
+      p(
+        em(
+          "A milestone date in The Netherlands is",
+          br(),
+          "2020-03-15: start social distancing"
+        )
+      ),
+      selectizeInput(
+        inputId = 'ref_date',
+        # do not change name
+        label = 'Milestone date in first country:',
+        choices = NULL,
+        options = list(
+          placeholder = 'e.g. 2020-03-15',
+          onInitialize = I('function() { this.setValue(""); }')
+        )
+      )
+    ),
+    column(
+      4,
+      p(
+        em(
+          "A milestone date in China is",
+          br(),
+          "2020-01-23: lockdown of Wuhan"
+        )
+      ),
+      selectizeInput(
+        inputId = 'query_date',
+        # do not change name
+        label = 'Milestone date in second country:',
+        choices = NULL,
+        options = list(
+          placeholder = 'e.g. 2020-01-23',
+          onInitialize = I('function() { this.setValue(""); }')
+        )
+      )
+    )),
+    
+    # Compare_Button
+    fluidRow(actionButton("goCompare", "Go!"))
+  ),
+  br(),
+  br(),
+  
+  # Compare_Output
+  fluidRow(plotOutput("plotCompare")),
+  br(),
+  
+  p(
+    em(
+      "Source data: European Center for Disease Control and Prevention (ECDC)"
+    )
+  ),
+  br()
+  
+)
